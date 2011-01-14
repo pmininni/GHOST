f70b3abaadeadade1395585f4afb220205273dd5
Duane Rosenberg <duane.rosenberg@noaa.gov>
2011-01-14 22:21:39 +0000

    utilities directory. First check-in.

---
+++ 3D/src/utils/gutils_mod.f90
@@ -0,0 +1,53 @@
+!=================================================================
+! GHOST suite Fortran utilities 
+!
+! 2010 D. Rosenberg
+!      NCAR
+!
+! 17 Nov 2010: Initial version
+!=================================================================
+MODULE gutils
+! Utils data, if any:
+!
+! ...
+! end, member data
+!
+!
+! Methods:
+      CONTAINS
+
+      SUBROUTINE rarray_byte_swap(Rin, nin)
+!-----------------------------------------------------------------
+!
+! Performs endian conversion on array of floats
+!
+! Parameters
+!     Rin : artibrary-rank array whose values will be endian-swapped, returned
+!     nin : dimension of Rin
+!-----------------------------------------------------------------
+      USE mpivars
+      USE threads
+      USE fprecision
+
+      IMPLICIT NONE
+
+      INTEGER, INTENT(IN)                        :: nin
+      REAL(KIND=GP), INTENT(INOUT), DIMENSION(*) :: Rin
+
+      INTEGER :: i, ie0, ie1, j, k, m, nb
+
+      nb = 8  ! no. bits per byte
+
+      DO k = 1, nin
+          ie0 = TRANSFER(Rin(k), 0)
+          DO m = 1, GP
+             CALL MVBITS( ie0, (GP-m)*nb, nb, ie1, (m-1)*nb  )
+          END DO
+          Rin(k) = TRANSFER(ie1, 0.0)
+       END DO
+
+      RETURN
+
+      END SUBROUTINE rarray_byte_swap
+
+END MODULE gutils
