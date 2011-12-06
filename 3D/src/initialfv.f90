2dcb2e17d93192fae97dbdad747226308a6fa846
Duane Rosenberg <duane.rosenberg@noaa.gov>
2011-12-06 17:26:36 +0000

    somehow, default was changed to be a non-NULL forcing; I'm changing it back here to a NULL forcing....

--- 3D/src/initialfv.f90
+++ 3D/src/initialfv.f90
@@ -9,44 +9,14 @@
 ! end, the three components of the forcing in spectral
 ! space should be stored in the arrays fx, fy, and fz.
 
-! Superposition of Taylor-Green vortices
-!     kdn : minimum wave number
-!     kup : maximum wave number
+! Null mechanical forcing
 
-!$omp parallel do if (iend-ista.ge.nth) private (j,k)
       DO i = ista,iend
-!$omp parallel do if (iend-ista.lt.nth) private (k)
          DO j = 1,n
             DO k = 1,n
-               fz(k,j,i) = 0.0_GP
+               fx(k,j,i) = 0.
+               fy(k,j,i) = 0.
+               fz(k,j,i) = 0.
             END DO
          END DO
       END DO
-
-!$omp parallel do if (kend-ksta.ge.nth) private (j,i)
-      DO k = ksta,kend
-!$omp parallel do if (kend-ksta.lt.nth) private (i)
-         DO j = 1,n
-            DO i = 1,n
-
-            R1(i,j,k) = 0.0_GP
-            R2(i,j,k) = 0.0_GP
-
-            DO ki = kdn,kup
-               R1(i,j,k) = R1(i,j,k)+SIN(2*pi*ki*(real(i,kind=GP)-1)/ &
-                          real(n,kind=GP))*COS(2*pi*ki*(real(j,kind=GP)-1)/  &
-                          real(n,kind=GP))*COS(2*pi*ki*(real(k,kind=GP)-1)/  &
-                          real(n,kind=GP))
-               R2(i,j,k) = R2(i,j,k)-COS(2*pi*ki*(real(i,kind=GP)-1)/ &
-                          real(n,kind=GP))*SIN(2*pi*ki*(real(j,kind=GP)-1)/  &
-                          real(n,kind=GP))*COS(2*pi*ki*(real(k,kind=GP)-1)/  &
-                          real(n,kind=GP))
-            END DO
-
-            END DO
-         END DO
-      END DO
-
-      CALL fftp3d_real_to_complex(planrc,R1,fx,MPI_COMM_WORLD)
-      CALL fftp3d_real_to_complex(planrc,R2,fy,MPI_COMM_WORLD)
-      CALL normalize(fx,fy,fz,f0,1,MPI_COMM_WORLD)
