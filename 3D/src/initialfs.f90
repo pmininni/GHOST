07939ebacced144a743a716225f361246c3c078a
Pablo Mininni <mininni@df.uba.ar>
2012-02-01 21:13:04 +0000

    Updated the initialfs and initials files (with examples) to include cases in the description in which the scalar quantity is active (as in the Boussinesq solvers).

--- 3D/src/initialfs.f90
+++ 3D/src/initialfs.f90
@@ -1,13 +1,15 @@
-! External source of passive scalar.
+! External source of passive/active scalar.
+! The scalar can be passive (e.g., in the PHD solver) or 
+! active (as in the Boussinesq solvers). 
 ! This file contains the expression used for the external 
-! forcing for the passive scalar. You can use temporary real 
-! arrays R1-R3 of size (1:n,1:n,ksta:kend) and temporary 
-! complex arrays C1-C8 of size (n,n,ista:iend) to do 
-! intermediate computations. The variable s0 should control 
-! the global amplitude of the forcing, and variables 
+! forcing of the scalar quantity in many solvers. You can 
+! use temporary real arrays R1-R3 of size (1:n,1:n,ksta:kend) 
+! and temporary complex arrays C1-C8 of size (n,n,ista:iend) 
+! to do intermediate computations. The variable s0 should 
+! control the global amplitude of the forcing, and variables 
 ! mparam0-9 can be used to control the amplitudes of 
 ! individual terms. At the end, the forcing in spectral
-! space should be stored in the array fs
+! space should be stored in the array fs.
 
 ! Null source
 
--- 3D/src/initials.f90
+++ 3D/src/initials.f90
@@ -1,13 +1,15 @@
-! Initial condition for the passive scalar.
+! Initial condition for the passive/active scalar.
+! The scalar can be passive (e.g., in the PHD solver) or 
+! active (as in the Boussinesq solvers). 
 ! This file contains the expression used for the initial 
-! concentration of the passive scalar. You can use temporary 
+! concentration of the scalar. You can use temporary 
 ! real arrays R1-R3 of size (1:n,1:n,ksta:kend) and temporary 
 ! complex arrays C1-C8 of size (n,n,ista:iend) to do 
 ! intermediate computations. The variable c0 should control 
 ! the global amplitude of the concentration, and variables 
 ! cparam0-9 can be used to control the amplitudes of individual 
-! terms. At the end, the initial passive scalar in spectral 
-! space should be stored in the array th.
+! terms. At the end, the initial concentration of the scalar in 
+! spectral space should be stored in the array th.
 
 ! Null pasive scalar
 
