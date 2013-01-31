2d687ed90d20639c6452c91e6d1074e0cc5a9180
Duane Rosenberg <duane.rosenberg@noaa.gov>
2013-01-31 20:04:15 +0000

    debugged version....

--- 3D/src/utils/gtimer_mod.f90
+++ 3D/src/utils/gtimer_mod.f90
@@ -8,55 +8,52 @@
 !=================================================================
 MODULE gtimer
       IMPLICIT NONE
-!  Timer data, if any:
+!  Timer data
 !
-! ...
-! end, member data
-      INTEGER, PARAMETER                :: MAXLEVELS=128, GTNULLHANDLE=-1
-      INTEGER, PARAMETER                :: GT_WTIME=0, GT_CPUTIME=1, GT_OMPTIME=2
+      INTEGER, PARAMETER                :: MAXLEVELS=128
+      INTEGER, PARAMETER, PUBLIC        :: GTNULLHANDLE=-1
+      INTEGER, PARAMETER, PUBLIC        :: GT_WTIME=0, GT_CPUTIME=1, GT_OMPTIME=2
 
-      INTEGER                           :: ihandle_(0:MAXLEVELS-1)
-      INTEGER                           :: itype_(0:MAXLEVELS-1)
-      DOUBLE PRECISION                  :: t0_(0:MAXLEVELS-1) 
-      DOUBLE PRECISION                  :: t1_(0:MAXLEVELS-1) 
+      INTEGER                           :: ihandle_(0:MAXLEVELS-1)=GTNULLHANDLE
+      INTEGER                           :: itype_(0:MAXLEVELS-1)=GT_WTIME
+      DOUBLE PRECISION                  :: t0_(0:MAXLEVELS-1)=0.0D0 
+      DOUBLE PRECISION                  :: t1_(0:MAXLEVELS-1)=0.0D0 
+! end, member data
 
 !
 ! Methods:
-      PUBLIC                            :: GTStart, GTStop, GTAcc
-      PUBLIC                            :: GTGetTime, GTFreeHandle
-      PRIVATE                           :: GTbasic, GTGetHandle
+      PUBLIC                            :: GTStart, GTStop, GTStopFree, GTAcc, GTFreeHandle
+      PUBLIC                            :: GTGetTime
+      PRIVATE                           :: GTbasic
+      PRIVATE                           :: GTGetHandle
 !
 !
 ! Methods:
       CONTAINS
 
       
-      SUBROUTINE GTStart(handle,itype)
+      SUBROUTINE GTStart(ih,itype)
 !-----------------------------------------------------------------
 ! Initializes time level, returns handle to it if successful
 !
 ! Parameters
-!     handle : (returned), Integer handle to timer level (-1 if unsuccessful)
+!     ih     : (returned), Integer handle to timer level (-1 if unsuccessful)
 !     itype  : timer type to use. See GT PARAMETERS for valid types
 !-----------------------------------------------------------------
-      USE mpivars
-      USE threads
-      USE fprecision
-
       IMPLICIT NONE
 
-      INTEGER, INTENT(OUT)                       :: handle
-      INTEGER, INTENT (IN)                       :: itype
+      INTEGER, INTENT(INOUT)                     :: ih
+      INTEGER, INTENT   (IN)                     :: itype
 
-      INTEGER                                    :: ih
 
-      IF ( itype .NE. GT_WTIME .and. itype .NE. GT_CPUTIME ) THEN
-        WRITE(*,*) 'GTStart: invalid time type'
+      IF ( itype .LT. GT_WTIME .OR. itype .GT. GT_OMPTIME ) THEN
+        WRITE(*,*) 'GTStart: invalid time type: ', itype 
         STOP
       ENDIF
 
       ih = GTGetHandle()
-      IF ( ih .LT. 0 ) THEN
+      IF ( ih .LT. 0 .OR. ih .GE. MAXLEVELS ) THEN
+        WRITE(*,*) 'GTStart: ih=', ih
         WRITE(*,*) 'GTStart: invalid handle. Increase no. levels?'
         STOP
       ENDIF
@@ -79,10 +76,6 @@ MODULE gtimer
 !     ih : Input integer handle to timer level (checked for validity,
 !          but this won't affect elapsed time).
 !-----------------------------------------------------------------
-      USE mpivars
-      USE threads
-      USE fprecision
-
       IMPLICIT NONE
 
       INTEGER, INTENT(INOUT)                     :: ih
@@ -99,6 +92,35 @@ MODULE gtimer
       END SUBROUTINE GTStop
 !
 !
+      SUBROUTINE GTStopFree(ih)
+!-----------------------------------------------------------------
+! Finalizes time level, and computes elapsed time for specified handle..
+! The handle _is_ deleted with this call; caller is responsible for
+! deleting it.  Use for one-off timings, where you might want to 
+! re-use handle.
+!
+! Parameters
+!     ih : Input integer handle to timer level (checked for validity,
+!          but this won't affect elapsed time).
+!-----------------------------------------------------------------
+      IMPLICIT NONE
+
+      INTEGER, INTENT(INOUT)                     :: ih
+
+      IF ( ih .GE. 0 .and. ih .LT. MAXLEVELS ) THEN
+        t1_(ih) = GTbasic(itype_(ih)) - t0_(ih);
+        CALL GTFreeHandle(ih)
+      ELSE
+        WRITE(*,*) 'GTStop: invalid handle: ', ih
+        STOP
+      ENDIF
+
+      RETURN
+
+      END SUBROUTINE GTStopFree
+
+!
+!
 
       SUBROUTINE GTAcc(ih)
 !-----------------------------------------------------------------
@@ -113,19 +135,15 @@ MODULE gtimer
 !     ih : Input integer handle to timer level (checked for validity,
 !          but this won't affect elapsed time).
 !-----------------------------------------------------------------
-      USE mpivars
-      USE threads
-      USE fprecision
-
       IMPLICIT NONE
 
       INTEGER, INTENT(IN)                        :: ih
 
       IF ( ih .GE. 0 .and. ih .LT. MAXLEVELS ) THEN
-        t1_(ih) += GTbasic(itype_(ih)) - t0_(ih)
-        t0_(ih)  = GTbasic(itype_(ih))
+        t1_(ih) = t1_(ih) + (GTbasic(itype_(ih)) - t0_(ih))
+        t0_(ih) = GTbasic(itype_(ih))
       ELSE
-        WRITE(*,*) 'GTStop: invalid handle: ', handle
+        WRITE(*,*) 'GTStop: invalid handle: ', ih
         STOP
       ENDIF
       
@@ -143,7 +161,6 @@ MODULE gtimer
 !     ih : Input integer handle to timer level (checked for validity,
 !          but this won't affect elapsed time).
 !-----------------------------------------------------------------
-
       INTEGER  :: ih
 
       IF ( ih .LT. 0 .OR. ih .GE. MAXLEVELS ) THEN
@@ -161,22 +178,22 @@ MODULE gtimer
 !
 !-----------------------------------------------------------------
 
-      INTEGER  :: ih, j
+      INTEGER  :: j
 
       j = 0
       GTGetHandle = GTNULLHANDLE
 
-      DO WHILE ( j.LT. MAXLEVELS .AND. ihandle_(j) .LT. 0 )
+      DO WHILE ( j.LT.MAXLEVELS .AND. ihandle_(j) .GE. 0 )
         j = j + 1
       ENDDO 
       IF ( j.LT.MAXLEVELS ) THEN
-        GTGetHandle = ih
+        GTGetHandle = j
       ENDIF
 
       END FUNCTION GTGetHandle
 !
 !
-      INTEGER FUNCTION GTFreeHandle(ih)
+      SUBROUTINE GTFreeHandle(ih)
 !-----------------------------------------------------------------
 ! Frees up all data associated with handle ih, and nullifies it
 !
@@ -189,15 +206,16 @@ MODULE gtimer
       INTEGER, INTENT(INOUT)  :: ih
 
       IF ( ih .LT. 0 .OR. ih .GE. MAXLEVELS ) THEN
-        WRITE(*,*) 'GTFreeHandle: invalid handle: ', ih
-        STOP
+!       WRITE(*,*) 'GTFreeHandle: invalid handle: ', ih
+!       STOP
+        RETURN
       ENDIF
       ihandle_(ih) = GTNULLHANDLE
       t0_     (ih) = 0.0D0
       t1_     (ih) = 0.0D0
       itype_  (ih) = 0
 
-      END FUNCTION GTFreeHandle
+      END SUBROUTINE GTFreeHandle
 !
 !
       DOUBLE PRECISION FUNCTION GTbasic(itype)
@@ -207,17 +225,17 @@ MODULE gtimer
 ! Parameters
 !     itype : time type
 !-----------------------------------------------------------------
-      INCLUDE 'mpif.h'
       IMPLICIT NONE
-
+      INCLUDE 'mpif.h'
+!$    DOUBLE PRECISION, EXTERNAL :: omp_get_wtime
       INTEGER, INTENT(IN)  :: itype
       DOUBLE PRECISION     :: tt
 
       SELECTCASE(itype)
         CASE(0)
-          GTbasic = MPI_WTIME()
+          tt = MPI_WTIME()
         CASE(1)
-          CALL CPU_Time(GTbasic)
+          CALL CPU_Time(tt)
         CASE(2)
          tt = 0.0D0
 !$       tt = omp_get_wtime()
@@ -225,6 +243,7 @@ MODULE gtimer
           WRITE(*,*) 'GTbasic: invalid time type'
           STOP
       END SELECT
+      GTBasic = tt
 
       END FUNCTION GTbasic
 !
