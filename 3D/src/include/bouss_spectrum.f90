1edef3258a870c1700fae77a926cc5bd56023e19
Pablo Mininni <mininni@df.uba.ar>
2016-05-24 18:24:04 +0000

    Commented out optional computation of spectra in the BOUSS solver, to have the default computing only a minimum number of spectra. Note also that one of the optional spectra computed there requiere 'omega' to be defined, and thus only works with the ROTBOUSS solver.

--- 3D/src/include/bouss_spectrum.f90
+++ 3D/src/include/bouss_spectrum.f90
@@ -9,17 +9,15 @@
             CALL spec2D(vx,vy,vz,ext,odir,1,1)
             CALL specsc2D(th,ext,odir,0)
 !
-            CALL havgwrite(0,'shear'  ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg shear
-            CALL havgwrite(1,'tgradz' ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg dtheta/dz
-            CALL havgwrite(2,'hawdtdz',ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg u_z * dtheta/dz
-            CALL havgwrite(3,'hahke'  ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg hor. k.e.
-            CALL havgwrite(4,'havke'  ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg vert. k.e.
-            CALL havgwrite(5,'haphel' ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg perp. helicity
-            CALL havgwrite(6,'haomzt' ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg ometa_z * theta
-            CALL havgwrite(7,'hapv2'  ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg pot'l vorticity^2
-            CALL havgwrite(8,'hasuph' ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg super-helicity
-            CALL havgwrite(9,'hari'   ,ext,vx,vy,vz,th,omega,bvfreq)  ! hor. avg Richardson no.
-
+!           CALL havgwrite(0,'shear'  ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg shear
+!           CALL havgwrite(1,'tgradz' ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg dtheta/dz
+!           CALL havgwrite(2,'hawdtdz',ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg u_z * dtheta/dz
+!           CALL havgwrite(3,'hahke'  ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg hor. k.e.
+!           CALL havgwrite(4,'havke'  ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg vert. k.e.
+!           CALL havgwrite(5,'haphel' ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg perp. helicity
+!           CALL havgwrite(6,'haomzt' ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg ometa_z * theta
+!           CALL havgwrite(7,'hapv2'  ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg pot'l vorticity^2
+!           CALL havgwrite(8,'hasuph' ,ext,vx,vy,vz,th,0.0_GP,bvfreq) ! hor. avg super-helicity
+!           CALL havgwrite(9,'hari'   ,ext,vx,vy,vz,th,omega,bvfreq)  ! hor. avg Richardson no.
 !           CALL spectpv(vx,vy,vz,th,ext)
-
 !           CALL dopdfbouss(vx,vy,vz,th,0.0_GP,bvfreq,ext,bpdf,nbins,bfixext,pdfext,dolog,C1,C2,C3,C4,R1,R2,R3)
