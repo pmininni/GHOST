7fff1c76e34340bbcf95bc17bf128796b84a35ae
Pablo Mininni <mininni@df.uba.ar>
2018-11-07 11:47:15 -0300

    Added computation of vorticity for inertial particles

--- 3D/src/include/inerpartout_velfield.f90
+++ 3D/src/include/inerpartout_velfield.f90
@@ -44,3 +44,24 @@
 
 ! Write velocity of inertial particles
            CALL lagpart%io_write_pdbv(1,odir,'vip',lgext,(t-1)*dt)
+
+! Write vorticity at particle positions
+!$omp parallel do if (iend-ista.ge.nth) private (j,k)
+           DO i = ista,iend
+!$omp parallel do if (iend-ista.lt.nth) private (k)
+              DO j = 1,ny
+                 DO k = 1,nz
+                    C1(k,j,i) = vx(k,j,i)*rmp
+                    C2(k,j,i) = vy(k,j,i)*rmp
+                    C3(k,j,i) = vz(k,j,i)*rmp
+                 END DO
+              END DO
+           END DO
+           CALL rotor3(C2,C3,C4,1)
+           CALL rotor3(C1,C3,C5,2)
+           CALL rotor3(C1,C2,C6,3)
+           CALL fftp3d_complex_to_real(plancr,C4,R1,MPI_COMM_WORLD)
+           CALL fftp3d_complex_to_real(plancr,C5,R2,MPI_COMM_WORLD)
+           CALL fftp3d_complex_to_real(plancr,C6,R3,MPI_COMM_WORLD)
+           CALL lagpart%SetLagVec(R1,R2,R3,.false.,R4,R5)
+           CALL lagpart%io_write_vec(1,odir,'wip',lgext,(t-1)*dt)
