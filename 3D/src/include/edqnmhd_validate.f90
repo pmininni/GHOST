ecc38c5185fdde6dcfbdb60fbe77843becb8426f
Pablo Mininni <mininni@df.uba.ar>
2020-01-21 15:14:22 -0300

    Improved validation test for anisotropic grids

--- 3D/src/include/edqnmhd_validate.f90
+++ 3D/src/include/edqnmhd_validate.f90
@@ -2,7 +2,7 @@
 ! are compatible with the solver used. If empty, the solver is
 ! compatible with all possible configurations of GHOST.
 
-      IF ( (anis.eq.1).or.(nx.ne.ny).or.(nx.ne.nz).or.(ny.ne.nz) ) THEN
+      IF (anis.eq.1) THEN
         IF (myrank.eq.0) THEN
            PRINT *,'EDQNM solvers require (2.pi)^3 cubic boxes.'
            PRINT *,'Please compile with ARBSIZE = no, and with nx = ny = nz.'
