b0fec5b9c95f41460601f7bdcacc61164f7f55bd
Pablo Mininni <mininni@df.uba.ar>
2020-07-05 18:46:43 -0300

    TG forcing should be the default forcing for tests and new users

--- 3D/src/initialfv.f90
+++ 3D/src/initialfv.f90
@@ -1,139 +1,58 @@
 ! External mechanical forcing.
 ! This file contains the expression used for the external
-! mechanical forcing. You can use temporary real arrays
-! R1-R3 of size (1:nx,1:ny,ksta:kend) and temporary complex
-! arrays C1-C8 of size (nz,ny,ista:iend) to do intermediate
+! mechanical forcing. You can use temporary real arrays R1-R3
+! of size (1:nx,1:ny,ksta:kend) and temporary complex arrays 
+! C1-C8 of size (1:nz,1:ny,ista:iend) to do intermediate
 ! computations. The variable f0 should control the global
 ! amplitude of the forcing, and variables fparam0-9 can be
 ! used to control the amplitudes of individual terms. At the
 ! end, the three components of the forcing in spectral
 ! space should be stored in the arrays fx, fy, and fz.
 
-! Superposition of harmonic modes with random phases
-!     kdn : minimum wave number
-!     kup : maximum wave number
+! Superposition of Taylor-Green vortices in an isotropic box
+!     kdn : minimum wave number (rounded to next integer)
+!     kup : maximum wave number (rounded to next integer)
 
-      IF (ista.eq.1) THEN
-         C1(1,1,1) = 0.
-         C2(1,1,1) = 0.
-         C3(1,1,1) = 0.
-         DO j = 2,ny/2+1
-
-            IF ((kk2(1,j,1).le.kup**2).and.(kk2(1,j,1).ge.kdn**2)) THEN
-               dump = 1./sqrt(kk2(1,j,1))
-               phase = 2*pi*randu(seed)
-               C1(1,j,1) = (COS(phase)+im*SIN(phase))*dump
-               C1(1,ny-j+2,1) = conjg(C1(1,j,1))
-               phase = 2*pi*randu(seed)
-               C2(1,j,1) = (COS(phase)+im*SIN(phase))*dump
-               C2(1,ny-j+2,1) = conjg(C2(1,j,1))
-               phase = 2*pi*randu(seed)
-               C3(1,j,1) = (COS(phase)+im*SIN(phase))*dump
-               C3(1,ny-j+2,1) = conjg(C3(1,j,1))
-            ELSE
-               C1(1,j,1) = 0.
-               C1(1,ny-j+2,1) = 0.
-               C2(1,j,1) = 0.
-               C2(1,ny-j+2,1) = 0.
-               C3(1,j,1) = 0.
-               C3(1,ny-j+2,1) = 0.
-            ENDIF
-
-         END DO
-         DO k = 2,nz/2+1
-
-            IF ((kk2(k,1,1).le.kup**2).and.(kk2(k,1,1).ge.kdn**2)) THEN
-               dump = 1./sqrt(kk2(k,1,1))
-               phase = 2*pi*randu(seed)
-               C1(k,1,1) = (COS(phase)+im*SIN(phase))*dump
-               C1(nz-k+2,1,1) = conjg(C1(k,1,1))
-               phase = 2*pi*randu(seed)
-               C2(k,1,1) = (COS(phase)+im*SIN(phase))*dump
-               C2(nz-k+2,1,1) = conjg(C2(k,1,1))
-               phase = 2*pi*randu(seed)
-               C3(k,1,1) = (COS(phase)+im*SIN(phase))*dump
-               C3(nz-k+2,1,1) = conjg(C3(k,1,1))
-            ELSE
-               C1(k,1,1) = 0.
-               C1(nz-k+2,1,1) = 0.
-               C2(k,1,1) = 0.
-               C2(nz-k+2,1,1) = 0.
-               C3(k,1,1) = 0.
-               C3(nz-k+2,1,1) = 0.
-            ENDIF
-
-         END DO
-         DO j = 2,ny
-            DO k = 2,nz/2+1
-     
-            IF ((kk2(k,j,1).le.kup**2).and.(kk2(k,j,1).ge.kdn**2)) THEN
-               dump = 1./sqrt(kk2(k,j,1))
-               phase = 2*pi*randu(seed)
-               C1(k,j,1) = (COS(phase)+im*SIN(phase))*dump
-               C1(nz-k+2,ny-j+2,1) = conjg(C1(k,j,1))
-               phase = 2*pi*randu(seed)
-               C2(k,j,1) = (COS(phase)+im*SIN(phase))*dump
-               C2(nz-k+2,ny-j+2,1) = conjg(C2(k,j,1))
-               phase = 2*pi*randu(seed)
-               C3(k,j,1) = (COS(phase)+im*SIN(phase))*dump
-               C3(nz-k+2,ny-j+2,1) = conjg(C3(k,j,1))
-            ELSE
-               C1(k,j,1) = 0.
-               C1(nz-k+2,ny-j+2,1) = 0.
-               C2(k,j,1) = 0.
-               C2(nz-k+2,ny-j+2,1) = 0.
-               C3(k,j,1) = 0.
-               C3(nz-k+2,ny-j+2,1) = 0.
-            ENDIF
-
-            END DO
-         END DO
-         DO i = 2,iend
-            DO j = 1,ny
-               DO k = 1,nz
-
-               IF ((kk2(k,j,i).le.kup**2).and.(kk2(k,j,i).ge.kdn**2)) THEN
-                  dump = 1./sqrt(kk2(k,j,i))
-                  phase = 2*pi*randu(seed)
-                  C1(k,j,i) = 2*(COS(phase)+im*SIN(phase))*dump
-                  phase = 2*pi*randu(seed)
-                  C2(k,j,i) = 2*(COS(phase)+im*SIN(phase))*dump
-                  phase = 2*pi*randu(seed)
-                  C3(k,j,i) = 2*(COS(phase)+im*SIN(phase))*dump
-               ELSE
-                  C1(k,j,i) = 0.
-                  C2(k,j,i) = 0.
-                  C3(k,j,i) = 0.
-               ENDIF
-
-               END DO
-            END DO
-         END DO
-      ELSE
-         DO i = ista,iend
-            DO j = 1,ny
-               DO k = 1,nz
-
-               IF ((kk2(k,j,i).le.kup**2).and.(kk2(k,j,i).ge.kdn**2)) THEN
-                  dump = 1./sqrt(kk2(k,j,i))
-                  phase = 2*pi*randu(seed)
-                  C1(k,j,i) = 2*(COS(phase)+im*SIN(phase))*dump
-                  phase = 2*pi*randu(seed)
-                  C2(k,j,i) = 2*(COS(phase)+im*SIN(phase))*dump
-                  phase = 2*pi*randu(seed)
-                  C3(k,j,i) = 2*(COS(phase)+im*SIN(phase))*dump
-               ELSE
-                  C1(k,j,i) = 0.
-                  C2(k,j,i) = 0.
-                  C3(k,j,i) = 0.
-               ENDIF
-
-               END DO
-            END DO
-        END DO
+      IF ( abs(Lx-Ly).gt.tiny ) THEN
+        IF (myrank.eq.0) &
+           PRINT *,'TG forcing require at least Lx=Ly'
+        STOP
       ENDIF
 
-      CALL rotor3(C2,C3,fx,1)
-      CALL rotor3(C1,C3,fy,2)
-      CALL rotor3(C1,C2,fz,3)
+!$omp parallel do if (iend-ista.ge.nth) private (j,k)
+      DO i = ista,iend
+!$omp parallel do if (iend-ista.lt.nth) private (k)
+         DO j = 1,ny
+            DO k = 1,nz
+               fz(k,j,i) = 0.0_GP
+            END DO
+         END DO
+      END DO
+
+!$omp parallel do if (kend-ksta.ge.nth) private (j,i)
+      DO k = ksta,kend
+!$omp parallel do if (kend-ksta.lt.nth) private (i)
+         DO j = 1,ny
+            DO i = 1,nx
+
+            R1(i,j,k) = 0.0_GP
+            R2(i,j,k) = 0.0_GP
+
+            DO ki = INT(kdn),INT(kup)
+               R1(i,j,k) = R1(i,j,k)+SIN(2*pi*ki*(real(i,kind=GP)-1)/ &
+                          real(nx,kind=GP))*COS(2*pi*ki*(real(j,kind=GP)-1)/  &
+                          real(ny,kind=GP))*COS(2*pi*ki*(real(k,kind=GP)-1)/  &
+                          real(nz,kind=GP))
+               R2(i,j,k) = R2(i,j,k)-COS(2*pi*ki*(real(i,kind=GP)-1)/ &
+                          real(nx,kind=GP))*SIN(2*pi*ki*(real(j,kind=GP)-1)/  &
+                          real(ny,kind=GP))*COS(2*pi*ki*(real(k,kind=GP)-1)/  &
+                          real(nz,kind=GP))
+            END DO
+
+            END DO
+         END DO
+      END DO
+
+      CALL fftp3d_real_to_complex(planrc,R1,fx,MPI_COMM_WORLD)
+      CALL fftp3d_real_to_complex(planrc,R2,fy,MPI_COMM_WORLD)
       CALL normalize(fx,fy,fz,f0,1,MPI_COMM_WORLD)
