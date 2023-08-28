c391c3551ad008860b38a2310290025b856e8c0e
Facundo Pugliese <fpugliese@df.uba.ar>
2023-08-28 15:24:32 -0300

    Added HPIC solver include files

--- 3D/src/include/hpic_global.f90
+++ 3D/src/include/hpic_global.f90
@@ -1,2 +1,3 @@
-            CALL picpart%GetTemperature(R1)
-            
+            CALL picpart%GetDensity(R1)
+            CALL picpart%GetTemperature(R2)
+            CALL hpiccheck(R2,R1,ax,ay,az,gammae,betae,t,dt)
