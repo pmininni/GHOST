65a5aae85f305d54bd1cd2763dced3535647485d
Facundo Pugliese <fpugliese@df.uba.ar>
2023-08-28 15:18:46 -0300

    Reworked ehpiccheck according to new parameters

--- 3D/src/include/ehpic_global.f90
+++ 3D/src/include/ehpic_global.f90
@@ -1,7 +1,2 @@
             CALL picpart%GetTemperature(R1)
-
-            CALL derivk3(phi,C1,1)
-            CALL derivk3(phi,C2,2)
-            CALL derivk3(phi,C3,3)
-
             CALL ehpiccheck(R1,rhoc,phi,t,dt)
