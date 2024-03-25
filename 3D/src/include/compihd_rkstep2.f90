2786902e45aa839f84eff61071dcb7d460efb984
duanerosenberg-zz <duaner62@gmail.com>
2024-03-25 21:30:56 +0000

    Use correct tmp arrays

--- 3D/src/include/compihd_rkstep2.f90
+++ 3D/src/include/compihd_rkstep2.f90
@@ -2,15 +2,15 @@
 ! Computes the nonlinear terms and evolves the equations in dt/o
 
          CALL prodre3(vx,vy,vz,C4,C5,C6)          ! om x v
-         CALL gradpressi(gam1,e,vx,vy,vz,C25,C26,C27) ! Grad (p+0.5v^2) term
+         CALL gradpressi(gam1,th,vx,vy,vz,C21,C22,C23) ! Grad (p+0.5v^2) term
          CALL vdiss(nu,nu2,vx,vy,vz)              ! viscous term
          CALL divide(rho,vx,vy,vz)                ! viscous term with rho
-         CALL divide(rho,C25,C26,C27)             ! divide Grad(p_tot) by rho
+         CALL divide(rho,C21,C22,C23)             ! divide Grad(p_tot) by rho
 
          CALL divrhov(rho,vx,vy,vz,C7)            ! div(rho.v)
 
          CALL divrhov(th,vx,vy,vz,C8)             ! div(e.v)
-         CALL pdVwork(gam1,th,vx,vy,vz,C9)        ! p.div(v) = (gamma-1) e.div(v)
+         CALL pdVwork(gam1,th,vx,vy,vz,C24)       ! p.div(v) = (gamma-1) e.div(v)
          CALL laplak3(th,th)                      ! laplacian(e)
         
          rmp = 1./real(o,kind=GP)
@@ -21,14 +21,14 @@
          DO k = 1,nz
 
             IF (kn2(k,j,i).le.kmax) THEN
-               vx (k,j,i) = C1(k,j,i)+dt*(vx(k,j,i)-C4(k,j,i)-C25(k,j,i) &
+               vx (k,j,i) = C1(k,j,i)+dt*(vx(k,j,i)-C4(k,j,i)-C21(k,j,i) &
               +fx(k,j,i))*rmp
-               vy (k,j,i) = C2(k,j,i)+dt*(vy(k,j,i)-C5(k,j,i)-C26(k,j,i) &
+               vy (k,j,i) = C2(k,j,i)+dt*(vy(k,j,i)-C5(k,j,i)-C22(k,j,i) &
               +fy(k,j,i))*rmp
-               vz (k,j,i) = C3(k,j,i)+dt*(vz(k,j,i)-C6(k,j,i)-C27(k,j,i) &
+               vz (k,j,i) = C3(k,j,i)+dt*(vz(k,j,i)-C6(k,j,i)-C23(k,j,i) &
               +fz(k,j,i))*rmp
                rho(k,j,i) = C20(k,j,i)-dt*C7(k,j,i)*rmp
-               th (k,j,i) = C21(k,j,i)+dt*(kappa*e(k,j,i)-C8(k,j,i)-C9(k,j,i))*rmp
+               th (k,j,i) = C21(k,j,i)+dt*(kappa*th(k,j,i)-C8(k,j,i)-C24(k,j,i))*rmp
             ELSE
                vx (k,j,i) = 0.
                vy (k,j,i) = 0.
