b371db5880224f62015d9a0656432293ee9a40a2
duanerosenberg-zz <duaner62@gmail.com>
2024-03-25 11:47:09 -0600

    First commit of compmihd_global include code for COMPIHD_SOL solver

---
+++ 3D/src/include/compihd_global.f90
@@ -0,0 +1,10 @@
+! Global quantities computed in compressible HD runs
+
+            CALL massenergycompi (vx,vy,vz,rho,e,t,dt)
+            CALL hdcheck(vx,vy,vz,fx,fy,fz,t,dt,1,1)
+            CALL maxabs(vx,vy,vz,rmp,0)
+            IF (myrank.eq.0) THEN
+               OPEN(1,file='maximum.txt',position='append')
+               WRITE(1,FMT='(E13.6,E13.6)') (t-1)*dt,rmp
+               CLOSE(1)
+            ENDIF
