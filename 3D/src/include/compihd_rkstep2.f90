8a4231e699014120e52eeedcc804e77b41117c50
duanerosenberg-zz <duaner62@gmail.com>
2024-03-26 18:27:06 +0000

    Remove debug output

--- 3D/src/include/compihd_rkstep2.f90
+++ 3D/src/include/compihd_rkstep2.f90
@@ -2,9 +2,9 @@
 ! Computes the nonlinear terms and evolves the equations in dt/o
 
          CALL prodre3(vx,vy,vz,C4,C5,C6)          ! om x v
-         CALL gradpressi(gam1,th,vx,vy,vz,C31,C32,323) ! Grad (p+0.5v^2) term
+         CALL gradpressi(gam1,th,vx,vy,vz,C31,C32,C33) ! Grad (p+0.5v^2) term
          CALL vdiss(nu,nu2,vx,vy,vz)              ! viscous term
-         CALL divide(rho,vx,vy,vz)                ! viscous term with rho
+         CALL divide(rho,vx,vy,vz)                ! divide viscous term by rho
          CALL divide(rho,C31,C32,C33)             ! divide Grad(p_tot) by rho
 
          CALL divrhov(rho,vx,vy,vz,C7)            ! div(rho.v)
