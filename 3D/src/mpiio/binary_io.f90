e3e0d805588f606134412406cb30eccd6a28b4ed
Duane.Rosenberg <duane.rosenberg@noaa.gov>
2025-08-03 03:01:43 +0000

    Add checks of NULL communicator

--- 3D/src/mpiio/binary_io.f90
+++ 3D/src/mpiio/binary_io.f90
@@ -36,6 +36,8 @@
       INTEGER, DIMENSION(3) :: subsizes,starts
       TYPE(IOPLAN), INTENT(OUT) :: plan
 
+      plan%comm   = MPI_COMM_WORLD
+      plan%myrank = myrank
       plan%nx = n(1)
       plan%ny = n(2)
       plan%nz = n(3)
@@ -60,7 +62,7 @@
 
 
 !*****************************************************************
-      SUBROUTINE io_init_comm(comm,n,ksta,kend,plan)
+      SUBROUTINE io_init_comm(comm,n,ksta,kend,nt,ktsta,ktend,plan)
 !-----------------------------------------------------------------
 !
 ! Initializes variables for MPI I/O. Creates plans and MPI 
@@ -74,6 +76,9 @@
 !     n     : the size of the dimensions of the input array [IN]
 !     ksta  : start value of the block in the third dimension [IN]
 !     kend  : end value of the block in the third dimension [IN]
+!     nt    : the size of the dimensions of the truncated input array [IN]
+!     ktsta : start value of the block in the truncated third dimension [IN]
+!     ktend : end value of the block in the truncated third dimension [IN]
 !     plan  : contains the I/O plan [OUT]
 !-----------------------------------------------------------------
 
@@ -83,20 +88,35 @@
       USE gtimer
       IMPLICIT NONE
 
-      INTEGER, INTENT(IN)   :: comm,n(3)
-      INTEGER, INTENT(IN)   :: ksta,kend
+      INTEGER, INTENT(IN)   :: comm,n(3), nt(3)
+      INTEGER, INTENT(IN)   :: ksta,kend,ktsta,ktend
       INTEGER, DIMENSION(3) :: subsizes,starts
+      INTEGER               :: ierr
       TYPE(IOPLAN), INTENT(OUT) :: plan
 
-      plan%nx   = n(1)
-      plan%ny   = n(2)
-      plan%nz   = n(3)
-      plan%ksta = ksta
-      plan%kend = kend
-      plan%comm = comm
+      IF ( comm .EQ. MPI_COMM_NULL ) THEN
+        ! On parent communicator
+        plan%comm   = MPI_COMM_NULL
+        plan%myrank = -1
+        plan%nx   = nt(1) ! will always call on truncated grid
+        plan%ny   = nt(2)
+        plan%nz   = nt(3)
+        plan%ksta = ktsta
+        plan%kend = ktend
+      ELSE
+        CALL MPI_COMM_DUP(comm, plan%comm, ierr)
+        CALL MPI_COMM_RANK(plan%comm, plan%myrank, ierr)
+        plan%nx   = nt(1)
+        plan%ny   = nt(2)
+        plan%nz   = nt(3)
+        plan%ksta = ktsta
+        plan%kend = ktend
 
-      subsizes(1) = n(1)
-      subsizes(2) = n(2)
+      ENDIF
+
+
+      subsizes(1) = nt(1)
+      subsizes(2) = nt(2)
       subsizes(3) = plan%kend-plan%ksta+1
       starts  (1) = 0
       starts  (2) = 0
@@ -138,16 +158,25 @@
       USE gutils
       IMPLICIT NONE
       
-      TYPE(IOPLAN),INTENT  (IN)      :: plan
-      REAL(KIND=GP),INTENT(OUT) :: var(plan%nx,plan%ny,plan%ksta:plan%kend)
+
       INTEGER, INTENT(IN)            :: unit
+      TYPE(IOPLAN),INTENT  (IN)      :: plan
       INTEGER                        :: fh
+      REAL(KIND=GP),INTENT(OUT)      :: var(plan%nx,plan%ny,plan%ksta:plan%kend)
       CHARACTER(len=100), INTENT(IN) :: dir
       CHARACTER(len=*), INTENT(IN)   :: nmb
       CHARACTER(len=*), INTENT(IN)   :: fname
 
+      IF ( plan%comm .EQ. MPI_COMM_NULL .OR. plan%myrank .LT. 0 ) THEN
+        RETURN
+      ENDIF
+
+
+      write(*,*) plan%myrank, ': io_read: 0'
+
       CALL GTStart(ihopen)
       IF ( bmangle.EQ.1 ) THEN
+      write(*,*) plan%myrank, ': io_read: 0.2: ', trim(dir) // '/' // fname //  '.' // nmb // '.out'
       CALL MPI_FILE_OPEN(plan%comm,trim(dir) // '/' // fname // &
           '.' // nmb // '.out',MPI_MODE_RDONLY,MPI_INFO_NULL,fh,ioerr)
       IF ( ioerr.NE.MPI_SUCCESS ) THEN
@@ -157,6 +186,7 @@
       ENDIF
       ELSE
 
+      write(*,*) plan%myrank, ': io_read: 0.3'
       CALL MPI_FILE_OPEN(plan%comm, trim(dir) // '/' // fname  &
                         ,MPI_MODE_RDONLY,MPI_INFO_NULL,fh,ioerr)
       IF ( ioerr.NE.MPI_SUCCESS ) THEN
@@ -168,17 +198,21 @@
       CALL GTStop(ihopen)
 
       CALL GTStart(ihread)
+      write(*,*) plan%myrank, ': io_read: 1'
       CALL MPI_FILE_SET_VIEW(fh,disp,GC_REAL,plan%iotype,'native', &
           MPI_INFO_NULL,ioerr)
+      write(*,*) plan%myrank, ': io_read: 2'
       CALL MPI_FILE_READ_ALL(fh,var, &
           plan%nx*plan%ny*(plan%kend-plan%ksta+1),GC_REAL,         &
           MPI_STATUS_IGNORE,ioerr)
+      write(*,*) plan%myrank, ': io_read: 3'
       CALL MPI_FILE_CLOSE(fh,ioerr)
       CALL GTStop(ihread)
       IF ( iswap.gt.0 ) THEN
         CALL rarray_byte_swap(var,plan%nx*plan%ny*(plan%kend-plan%ksta+1))
       ENDIF
-
+      write(*,*) plan%myrank, ': io_read: done.'
+      
       RETURN
       END SUBROUTINE io_read
 
@@ -208,13 +242,20 @@
 !     USE gutils
       IMPLICIT NONE
 
+
       TYPE(IOPLAN), INTENT(IN)       :: plan
-      REAL(KIND=GP), INTENT(INOUT) :: var(plan%nx,plan%ny,plan%ksta:plan%kend)
       INTEGER, INTENT(IN)            :: unit
       INTEGER                        :: fh
+      REAL(KIND=GP), INTENT(INOUT) :: var(plan%nx,plan%ny,plan%ksta:plan%kend)
       CHARACTER(len=100), INTENT(IN) :: dir
       CHARACTER(len=*), INTENT(IN)   :: nmb
       CHARACTER(len=*), INTENT(IN)   :: fname
+     
+      write(*,*) ' io_write: myrank=', plan%myrank, ' bmangle=', bmangle
+      IF ( plan%comm .EQ. MPI_COMM_NULL .OR. plan%myrank .LT. 0 ) THEN
+        RETURN
+      ENDIF
+
 
       CALL GTStart(ihopen)
       IF ( bmangle.EQ.1 ) THEN
@@ -235,6 +276,9 @@
           MPI_STATUS_IGNORE,ioerr)
       CALL MPI_FILE_CLOSE(fh,ioerr)
       CALL GTStop(ihwrite)
+
+!     CALL MPI_BARRIER(plan%comm,ierr)
+      write(*,*) ' io_write: myrank=', plan%myrank, ' done.'
 
       RETURN
       END SUBROUTINE io_write
