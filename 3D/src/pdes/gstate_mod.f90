987af6f06fb522a2159025cb7c1fb5e17fa9084a
Duane Rosenberg <duane.rosenberg@noaa.gov>
2025-12-09 22:21:26 -0700

    .

--- 3D/src/pdes/gstate_mod.f90
+++ 3D/src/pdes/gstate_mod.f90
@@ -1,15 +1,21 @@
-module gstate_types
+module gstate_mod
     use fprecision
+    use ali
+    use kes
+    use var
+    use grid
     use mpivars
+!$  use threads
 
     implicit none
 
     type, public :: GStateComp 
-      complex(kind=GP), pointer, dimension(:,:,:) :: ccomp ! complex component
-      real   (kind=GP), pointer, dimension(:,:,:) :: rcomp ! real component
+      complex(kind=GP), pointer, dimension(nz,ny,ista:iend) :: ccomp ! complex component
+      real   (kind=GP), pointer, dimension(nx,ny,ksta_kend) :: rcomp ! real component
     end type GStateComp
 
+  contains
 
-end module gstate_types
+end module gstate_mod
 
 
