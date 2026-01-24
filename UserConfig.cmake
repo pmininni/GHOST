a5216d2f943c1271fbe6ef41d722a5811a76b8d5
Pablo Mininni <mininni@df.uba.ar>
2026-01-24 11:11:53 -0300

    Removed nx,ny,nz and ord from the parameters defined at compile time

--- UserConfig.cmake
+++ UserConfig.cmake
@@ -6,16 +6,12 @@
 # "cmake .. -DCMAKE_PREFIX_PATH=/opt/openmpi-5.0.8 -DFFTW3_ROOT=/opt/fftw-3.3.10"
 # -------------------------------------------------------------------
 
-# ----------------------- Numeric/grid params ------------------------
-set(NX 64    CACHE STRING "Grid size in X")
-set(NY 64    CACHE STRING "Grid size in Y")
-set(NZ 64    CACHE STRING "Grid size in Z")
-set(ORD 2    CACHE STRING "Order")
+# ----------------------- Advanced set up params --------------------
 set(IKIND 8  CACHE STRING "Integer kind")
 set(CSIZE 8  CACHE STRING "Cache size")
 set(NSTRIP 1 CACHE STRING "Strip mining")
 
-# ----------------------- Options / variants -------------------------
+# ----------------------- Options / variants ------------------------
 option(ARBSIZE  "Enable arbitrary box sizes" OFF)
 option(P_HYBRID "Enable hybrid OpenMP"       OFF)
 set(SOLVER "HD"        CACHE STRING "Solver type (HD, MHD)")
