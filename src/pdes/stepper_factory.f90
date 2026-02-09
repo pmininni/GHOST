94419ee1af5dc18f3b3c05a97c9a6f17a70c8019
Duane Rosenberg <duane.rosenberg@noaa.gov>
2026-02-08 17:10:10 -0700

    First commit of stepper factory, guts of which were removed from EquationBase

---
+++ src/pdes/stepper_factory.f90
@@ -0,0 +1,81 @@
+! ===================================================================
+! Factory for all allowed time stepper objects
+!
+! To add a new solver, "use" the corresponding module, and add a new
+! case clause in the build_stepper_from_file function. The clause should
+! allocate the stepper class, and call its constructor
+!
+! DATE : 02/8/26 (DLR)
+! ===================================================================
+
+module stepper_factory_mod
+  use gstepperbase_mod
+  use gexrk_stepper_mod
+  use canuto_stepper_mod
+
+  
+  implicit none
+
+  ! ================= Global parameters ===============================
+  
+contains
+  
+  ! ================= Factory function ==============================
+  function build_stepper_from_file(infile,workspace) result(new_object)
+    use commtypes
+    class(GStepperBase), allocatable  :: new_object
+    character(len=*)   , intent(in)   :: infile
+    type   (GWorkspace), intent(inout), &
+                               target :: workspace
+
+
+    call MPI_COMM_SIZE(MPI_COMM_WORLD,nprocs,ierr)
+    call MPI_COMM_RANK(MPI_COMM_WORLD,myrank,ierr)
+
+    ! Temporary data to read from namelists:
+    integer                                :: itype,norder,nstage
+    integer                                :: ierr
+    character(len=128)                     :: sname ! stepper name
+    type(GStepperTraits)                   :: straits
+    type      (GStepper), allocatable      :: new_object
+
+    ! Required namelists:
+    namelist/ stepper    / sname, itype, norder, nstage
+
+    itype          = 1 ! Butcher type if using GEXRK object
+    norder         = 2
+    nstage         = 2
+    sname          = 'CANUTO'
+
+    if ( myrank .eq. 0 ) then
+      open(1,file=this%infile,status='unknown',form="formatted")
+      read(1,NML=stepper)
+      close(1)
+    endif
+    call mpi_bcast(sname    ,128 ,MPI_CHARACTER,0,MPI_COMM_WORLD,ierr)
+    call mpi_bcast(itype    ,1   ,MPI_INTEGER  ,0,MPI_COMM_WORLD,ierr)
+    call mpi_bcast(norder   ,1   ,MPI_INTEGER  ,0,MPI_COMM_WORLD,ierr)
+    call mpi_bcast(nstage   ,1   ,MPI_INTEGER  ,0,MPI_COMM_WORLD,ierr)
+
+    ! Build stepper traits structure:
+    straits.nstate = this%state_size()
+    straits.norder = norder
+    straits.nstage = nstage
+    straits.sname  = sname
+
+   ! Allocate child object:
+   select case (trim(to_lowercase(sname)))
+     case ('gexrk')
+       allocate( GExRKStepper  :: new_object )
+     case ('canuto')
+       allocate( CanutoStepper :: new_object )
+     case default
+       stop 'stepper_factory::build_stepper_from_file: Invalid stepper type'
+   end select
+
+   ! Call constructor:
+   new_object%GStepper_ctor(straits,workspace)
+
+  end function build_stepper_from_file
+
+end module stepper_factory_mod
