859f8707231ae0ccffbcdb22c90265abf67ca7dd
Pablo Mininni <mininni@df.uba.ar>
2026-03-28 15:23:18 -0300

    Removed calls to particles from the Boussinesq solver

--- src/pdes/bouss_solver.f90
+++ src/pdes/bouss_solver.f90
@@ -56,7 +56,6 @@ module bouss_mod
   ! ================= Solver traits ===================================
   type, public  :: BSTraits
     logical       :: dorot        = .FALSE. ! rotation flag
-    logical       :: doparts      = .FALSE. ! do particles flag
     integer       :: spectlod     = 1       ! standard level of spectra detail 
     real(kind=GP) :: nu           = 0.0_GP  ! dissipation
     real(kind=GP) :: bkappa       = 0.0_GP  ! diffusivity
@@ -77,7 +76,6 @@ module bouss_mod
   contains
     procedure, public :: init          =>          init_impl ! init method
     procedure, public :: dudt          =>          dudt_impl ! RHS method
-    procedure, public :: pdudt         =>         pdudt_impl ! part+field RHS method
     procedure, public :: global        =>        global_impl ! Writes global qtys
     procedure, public :: spectra       =>       spectra_impl ! Writes spectra
     procedure, public :: state_size    =>    state_size_impl ! state size
@@ -306,7 +304,6 @@ contains
       end do
     end do
 
-
 !$omp parallel do if (iend-ista.ge.nth) private (j,k)
     do i = ista,iend
 !$omp parallel do if (iend-ista.lt.nth) private (k)
@@ -340,158 +337,6 @@ contains
     call this%rhs_passive(uin, uf, this%traits_%kappa, dudt)
 
   end subroutine dudt_impl
-
-
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !! Function to compute part+field RHS
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  subroutine pdudt_impl(this, time, uin, puin, uf, dt, dudt, pdudt) 
-    use pseudospec_fluid
-    use ali
-    use kes
-    use var
-    use grid
-    use mpivars
-!$  use threads
-    implicit none
-
-    class  (BOUSSSolver), intent   (in)          :: this
-    real    (kind=GP), intent   (in)             :: time, dt
-    type( GStateComp), intent(inout), target     :: uin(:),uf(:)
-    type(GPStateComp), intent(inout), target     :: puin(:)
-    type (GStateComp), intent(inout)             :: dudt(:) 
-    type(GPStateComp), intent(inout)             :: pdudt(:) 
-    complex(kind=GP), pointer, dimension(:,:,:)  :: fx,fy,fz,vx,vy,vz
-    complex(kind=GP), pointer, dimension(:,:,:)  :: fth,th
-    complex(kind=GP), pointer, dimension(:,:,:)  :: C1,C2,C3,C4,C5,C6
-    complex(kind=GP), pointer, dimension(:,:,:)  :: C7,C8
-    real   (kind=GP)                             :: bkappa,bvfreq,nu
-    real   (kind=GP)                             :: xmom,xtemp
-    real   (kind=GP)                             :: omegax,omegay,omegaz
-    integer                                      :: i,j,k
-    logical                                      :: bret
-
-
-    stop 'BOUSSSolver::pdudt: Solver particle interface not ready yet!'
-
-    if ( .not. this%traits_%doparts) then
-      stop 'BOUSSolver::dud: Particle interface cannot be used'
-    endif
-
-    if ( .not. this%binit_ ) then
-      stop 'BOUSSolver::dud: Solver not initialized'
-    endif
-       
-    nu     = this%traits_%nu
-    bkappa = this%traits_%bkappa
-    bvfreq = this%traits_%bvfreq
-    xmom   = this%traits_%xmom  * this%bvfreq
-    xtemp  = this%traits_%xtemp * this%traits_%bvfreq
-
-    call this%workspace_%get_complex_tmp(C1,bret)
-    call this%workspace_%get_complex_tmp(C2,bret)
-    call this%workspace_%get_complex_tmp(C3,bret)
-    call this%workspace_%get_complex_tmp(C4,bret)
-    call this%workspace_%get_complex_tmp(C5,bret)
-    call this%workspace_%get_complex_tmp(C6,bret)
-    call this%workspace_%get_complex_tmp(C7,bret)
-    call this%workspace_%get_complex_tmp(C8,bret)
-
-    vx  => uin(this%VELOCITY  )%ccomp
-    vy  => uin(this%VELOCITY+1)%ccomp
-    vz  => uin(this%VELOCITY+2)%ccomp
-    th  => uin(this%TEMP)      %ccomp
-    fx  => uf (this%VELOCITY  )%ccomp
-    fy  => uf (this%VELOCITY+1)%ccomp
-    fz  => uf (this%VELOCITY+2)%ccomp
-    fth => uf (this%TEMP)      %ccomp
-      
-    call prodre3(vx,vy,vz,C4,C5,C6)                    ! w x v
-    if ( this%traits_%dorot ) then
-      omegax = this%traits_%omega(1)
-      omegay = this%traits_%omega(2)
-      omegaz = this%traits_%omega(3)
-      call saxpby_c(C1, vz, 2*omegay, vy, -2.0*omegaz) ! 2 Omega x v
-      call saxpby_c(C2, vx, 2*omegaz, vz, -2.0*omegax)
-      call saxpby_c(C3, vy, 2*omegax, vx, -2.0*omegay)
-!$omp parallel do if (iend-ista.ge.nth) private (j,k)
-      do i = ista,iend
-!$omp parallel do if (iend-ista.lt.nth) private (k)
-      do j = 1,ny
-      do k = 1,nz
-         C4(k,j,i) = C4(k,j,i) + C1(k,j,i) ! (w x v + 2 Omega x v)_x
-         C5(k,j,i) = C5(k,j,i) + C2(k,j,i) ! (w x v + 2 Omega x v)_y
-         C6(k,j,i) = C6(k,j,i) + C3(k,j,i) ! (w x v + 2 Omega x v)_z
-      end do
-      end do
-      end do
-    endif
-
-!$omp parallel do if (iend-ista.ge.nth) private (j,k)
-    do i = ista,iend
-!$omp parallel do if (iend-ista.lt.nth) private (k)
-    do j = 1,ny
-    do k = 1,nz
-       C6(k,j,i) = C6(k,j,i) - xmom*th(k,j,i) ! buoyancy term
-    end do
-    end do
-    end do
-
-    call nonlhd3(C4,C5,C6,C1,1)  ! -[(w + 2 Omega) x v + Grad p]_x
-    call nonlhd3(C4,C5,C6,C2,2)  ! -[(w + 2 Omega) x v + Grad p]_y
-    call nonlhd3(C4,C5,C6,C3,3)  ! -[(w + 2 Omega) x v + Grad p]_z
-    call laplak3(vx,C4)          ! Del^2 vx
-    call laplak3(vy,C5)          ! Del^2 vy
-    call laplak3(vz,C6)          ! Del^2 vz
-
-    call advect3(vx,vy,vz,th,C7) ! -(v.Grad) th
-    call laplak3(th,C8)          ! Del^2 th
-
-!$omp parallel do if (iend-ista.ge.nth) private (j,k)
-    do i = ista,iend               ! heat 'currrent':
-!$omp parallel do if (iend-ista.lt.nth) private (k)
-      do j = 1,ny
-        do k = 1,nz
-          C7(k,j,i) = C7(k,j,i) + xtemp*vz(k,j,i) ! add N vz term
-        end do
-      end do
-    end do
-
-
-!$omp parallel do if (iend-ista.ge.nth) private (j,k)
-    do i = ista,iend
-!$omp parallel do if (iend-ista.lt.nth) private (k)
-    do j = 1,ny
-    do k = 1,nz
-      if ((kn2(k,j,i).le.kmax).and.(kn2(k,j,i).ge.tiny)) then
-        dudt(this%VELOCITY  )%ccomp(k,j,i) = nu*C4(k,j,i) + C1(k,j,i) + fx(k,j,i)
-        dudt(this%VELOCITY+1)%ccomp(k,j,i) = nu*C5(k,j,i) + C2(k,j,i) + fy(k,j,i)
-        dudt(this%VELOCITY+2)%ccomp(k,j,i) = nu*C6(k,j,i) + C3(k,j,i) + fz(k,j,i)
-        dudt(this%TEMP)%ccomp(k,j,i) = bkappa*C8(k,j,i) + C7(k,j,i) + fth(k,j,i)
-      else
-        dudt(this%VELOCITY  )%ccomp(k,j,i) = 0.0_GP
-        dudt(this%VELOCITY+1)%ccomp(k,j,i) = 0.0_GP
-        dudt(this%VELOCITY+2)%ccomp(k,j,i) = 0.0_GP
-        dudt(this%TEMP)      %ccomp(k,j,i) = 0.0_GP
-      endif
-    enddo
-    enddo
-    enddo
-
-    call this%workspace_%free_complex_tmp(C1)
-    call this%workspace_%free_complex_tmp(C2)
-    call this%workspace_%free_complex_tmp(C3)
-    call this%workspace_%free_complex_tmp(C4)
-    call this%workspace_%free_complex_tmp(C5)
-    call this%workspace_%free_complex_tmp(C6)
-    call this%workspace_%free_complex_tmp(C7)
-    call this%workspace_%free_complex_tmp(C8)
-
-    ! Compute passive scalars:
-    call this%rhs_passive(uin, uf, this%traits_%kappa, dudt)
-
-  end subroutine pdudt_impl
-
 
   ! ===================================================================
   ! Computation of global quantities and spectra
